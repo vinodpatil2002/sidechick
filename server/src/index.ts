b6b230df68bf280d064627b699bcc5ed8f467005
vinodpatil2002 <vinodnspatil@gmail.com>
2024-11-16 23:51:46 +0530

    Introductory code

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import jwt from 'jsonwebtoken';
+
+
+
+const app = express();
+
+app.post("api/v1/signup",(req,res)=>{
+
+})
+
+
+app.post("api/v1/signin",(req,res)=>{
+    
+})
+
+app.post("api/v1/content",(req,res)=>{
+    
+})
+
+app.get("api/v1/content",(req,res)=>{
+    
+})
+
+
+app.post("api/v1/brain/share",(req,res)=>{
+    
+})
+
+
+app.get("api/v1/brain/:id",(req,res)=>{
+    
+})
+
+
